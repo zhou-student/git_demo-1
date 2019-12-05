3be6471bd4042a82fac353256b92b342ba0528e3
zhou-student <58451518+zhou-student@users.noreply.github.com>
2019-12-05 16:38:08 +0800

    Update settings.py

--- git_demo/settings.py
+++ git_demo/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import love you(**)
+print("老子跟你在一块一定真tm幸福")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
